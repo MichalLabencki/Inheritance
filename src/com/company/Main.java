31ccf78e6eec0726ccb48d4ddfa864c7e08338f6
MichalLabencki <74935701+MichalLabencki@users.noreply.github.com>
2021-01-15 15:21:27 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Animal animal = new Animal("Animal", 1 ,1,1,1);
+
+	Dog dog = new Dog("YoriE", 1, 1, 1, 1,1 ,1,"long silky");
+	dog.eat();
+    }
+}
